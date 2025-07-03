1a7ecf7c9fe7bbf7c885ab0fa18c72da039166ac
ImOhashi <leonardoohashi.mkt@gmail.com>
2025-07-02 22:18:46 -0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use std::os::unix::net::SocketAddr;
+
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let server_address = "0.0.0.0:3000";
+
+    let app = Router::new()
+                .route("/", get(hello_world));
+    
+    let listener = tokio::net::TcpListener::bind(&server_address).await.unwrap();
+
+    println!("Server running at {}", server_address);
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn hello_world() -> &'static str {
+    "Hello world!"
+}
